3c0fdff56cd8e31bbef658f769feaa31cef02584
kimlony <kshkjk8390@naver.com>
2026-07-19 18:02:23 +0900

    https 관련 cors문제 수정

--- hub-api-erp/src/main/java/hub/config/SecurityConfig.java
+++ hub-api-erp/src/main/java/hub/config/SecurityConfig.java
@@ -75,7 +75,10 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173"));
+        config.setAllowedOrigins(List.of(
+            "http://localhost:5173",
+            "https://hub.rony.kr"
+        ));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
